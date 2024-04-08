127c79b211a3ea6aef67017b7e4c245811ce17c0
Laszlo Nemeth <nemeth.rzr@gmail.com>
2024-04-08 13:06:21 +0200

    working

--- scripts.js
+++ scripts.js
@@ -1,19 +1,7 @@
-
-
 const shareBtn = document.querySelector('.share');
 const socialWindow = document.querySelector('.social');
-
-
-
 shareBtn.addEventListener('click', function () {
 
     socialWindow.classList.toggle('active');
 });
 
-function getOffset(el) {
-    const rect = el.getBoundingClientRect();
-    return {
-        left: rect.left + window.scrollX,
-        top: rect.top + window.scrollY
-    };
-}
